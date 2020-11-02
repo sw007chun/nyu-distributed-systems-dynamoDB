8c13a3216c509bf33baf909f606b8743e26fbd14
yd1568 <yd1568@nyu.eu>
2020-11-02 15:37:58 -0500

    Create a mix project

---
+++ proj/lib/proj.ex
@@ -0,0 +1,18 @@
+defmodule Proj do
+  @moduledoc """
+  Documentation for `Proj`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Proj.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

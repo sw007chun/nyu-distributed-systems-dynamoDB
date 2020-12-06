4b2357329dfffffecdae63fcc7645c1f90e25d42
yd1568 <yd1568@nyu.eu>
2020-12-05 23:52:56 -0500

    Add get timeout and cast update code

--- apps/dynamo/lib/dynamo/vnode.ex
+++ apps/dynamo/lib/dynamo/vnode.ex
@@ -108,11 +108,11 @@ defmodule Vnode do
   defp wait_read_response(key, value, index, context, state, current_read, num_read) do
     if current_read < num_read do
       receive do
-        {:ok, ^key, {other_value, other_context}, other_index} ->
+        {:ok, ^key, {other_value, other_context}, other_index, sender} ->
           cond do
             VClock.compare_vclocks(context, other_context) == :after ->
               Logger.info("vclock received. Result of comparison: After")
-              # Need to send a put request to the node behind
+              GenServer.cast({Vnode.Master, sender}, {:repl_command, Node.self, {:update_repl, key, value, index, context}})
               wait_read_response(key, value, index, context, state, current_read+1, num_read)
             VClock.compare_vclocks(context, other_context) == :before ->
               Logger.info("vclock received. Result of comparison: Before")
@@ -131,6 +131,10 @@ defmodule Vnode do
               # Need to send all responses as we have divergent branches
               wait_read_response(key, value, index, context, state, current_read+1, num_read)
           end
+        after
+          10_000 ->
+            Logger.info("Timed out while waiting for R replies")
+            {value, state}
       end
     else
       {value, state}
@@ -181,8 +185,19 @@ defmodule Vnode do
       |> Map.get(index, %{})
       |> Map.get(key, :key_not_found)
 
-    send(sender, {:ok, key, value, state.partition})
+    send(sender, {:ok, key, value, state.partition, Node.self()})
     {:noreply, state}
+  end
+
+  @impl true
+  def handle_cast({:update_repl, key, value, index, context}, state) do
+    {_, new_data} =
+      state.data
+      |> Map.get_and_update(index, fn index_store ->
+        {nil, Map.put(index_store, key, {value, context})}
+      end)
+
+    {:noreply, %{state | data: new_data}}
   end
 
   @impl true
